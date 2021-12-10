cbaf2c08f697d2cacaf5d65ca82e508f47566519
BreakBB <b.boeing@online.de>
2021-12-10 11:39:50 +0100

    Add checkToken endpoint

--- src/main/java/de/codecentric/bb/login/LoginController.java
+++ src/main/java/de/codecentric/bb/login/LoginController.java
@@ -1,6 +1,7 @@
 package de.codecentric.bb.login;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,5 +21,12 @@ public class LoginController {
     @PostMapping
     public TokenResponse login(@RequestBody LoginRequest loginRequest) {
         return loginService.handleLoginRequest(loginRequest.getUsername(), loginRequest.getPassword());
+    }
+
+    @GetMapping("/checkToken")
+    public boolean isValidAccessToken() {
+        // This endpoint is required to return something useful, when trying to verify if the access_token is still valid.
+        // The Keycloak configuration checks this endpoint before reaching this code and returns 401 if the token is not valid.
+        return true;
     }
 }
