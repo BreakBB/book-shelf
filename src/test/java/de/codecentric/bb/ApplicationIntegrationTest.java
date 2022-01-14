5f98a4d383a1e6c86a55f1af1446cf24ec4e3354
BreakBB <b.boeing@online.de>
2022-01-14 14:53:49 +0100

    Add test for application start

---
+++ src/test/java/de/codecentric/bb/ApplicationIntegrationTest.java
@@ -0,0 +1,11 @@
+package de.codecentric.bb;
+
+import org.junit.jupiter.api.Test;
+
+class ApplicationIntegrationTest {
+
+    @Test
+    void applicationStarts() {
+        Application.main(new String[]{});
+    }
+}
