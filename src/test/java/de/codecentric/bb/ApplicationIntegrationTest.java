1354cc9d240bfc5c5213376cbc7600b80644468d
BreakBB <b.boeing@online.de>
2022-02-11 14:41:09 +0100

    Fix application start test to use port different to default one

--- src/test/java/de/codecentric/bb/ApplicationIntegrationTest.java
+++ src/test/java/de/codecentric/bb/ApplicationIntegrationTest.java
@@ -1,11 +1,17 @@
 package de.codecentric.bb;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class ApplicationIntegrationTest {
 
     @Test
+    void contextLoads() {
+    }
+
+    @Test
     void applicationStarts() {
-        Application.main(new String[]{});
+        Application.main(new String[] {});
     }
 }
